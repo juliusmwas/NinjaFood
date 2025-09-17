c4ae70dfa6965d9c210e39699634a7e31f22c12b
juliusmwass <juliusmwass100@gmail.com>
2025-09-17 10:09:17 +0300

    Added a responsive nav bar using tailwind css

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,27 +1,30 @@
 import "tailwindcss"
+import { IoMenu } from "react-icons/io5";
 
 function Homepage () {
     return (
         <div>
-            <nav>
+            <nav className="flex p-4 justify-between items-center bg-white-500 shadow-sm "> 
                 <div>
-                    <h1>Ninja <span>Food</span></h1>
+                    <h1 className="text-2xl  font-roboto font-bold ">Ninja <span className="text-orange-400">Food</span></h1>
                 </div>
                 
-                <div>
-                    <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Contact Us</a></li>
+                <div> 
+                    <ul className="hidden lg:flex gap-10 font-roboto text-lg font-semibold">
+                        <li className=" hover:text-orange-600 "><a href="#">Home</a></li>
+                        <li className=" hover:text-orange-600 "><a href="#">About</a></li>
+                        <li className=" hover:text-orange-600 "><a href="#">Contact Us</a></li>
                     </ul>
                 </div>
 
-                <div>
-                    <button>Login</button>
-                    <button>Sign up</button>
+                <div className=" hidden lg:flex gap-4">
+                    <button className="border-2 px-5 rounded-md text-lg  cursor-pointer transition delay-150 duration-150 ease-in-out hover:bg-orange-600 hover:text-white hover:-translate-y-1 hover:scale-110 font-medium font-roboto">Login</button>
+                    <button className=" px-3 bg-orange-600 rounded-md text-lg  cursor-pointer transition delay-150 duration-150 ease-in-out hover:bg-orange-600 hover:text-white hover:-translate-y-1 hover:scale-110 font-medium font-roboto">Sign up</button>
                 </div>
+                
+                <IoMenu className="text-2xl text-orange-500 display-none block lg:hidden"/>
             </nav>
-            
+
             {/* End Nav bar */}
         </div>
     )
