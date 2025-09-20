557a3e5d0a1d58f6b57f8522ba5220e0743de76b
juliusmwass <juliusmwass100@gmail.com>
2025-09-20 13:24:15 +0300

    Added new content in dashboard page

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -12,7 +12,7 @@ function Dashboard() {
   return (
     <div className="flex">
       {/* Sidebar */}
-      <div className="fixed top-0 left-0 h-full w-64 w-64 bg-white shadow-xl">
+      <div className="fixed top-0 left-0 h-full w-64  bg-white shadow-xl">
         <nav className="p-4 font-roboto">
           <div className="flex items-center justify-between pb-6">
             <h1 className="text-2xl font-bold">HabitFlow</h1>
@@ -61,25 +61,29 @@ function Dashboard() {
         <div className="max-w-[1000px]">
           <div className="flex gap-4 mb-4">
 
-            <div className="bg-red-200 h-80 w-[700px] rounded flex gap-3 ">
-              <div className="block text-center ">
-                <h1>How to Build <br />a New Habit</h1>
-                <p>This is essential for making progress in your <br />health, happiness and your life</p>
-                <button className="bg-red-400 rounded p-1">+ Add New Habit</button>
-              </div>
-              <div>
-                <img src="./Colony.png" className="h-60" />
-              </div>
-            </div>
+            <div className=" bg-blue-300 h-80 w-[700px] rounded-xl flex items-center justify-between overflow-hidden">
+               
 
-            <div className="bg-red-200 h-80 w-[300px] rounded flex items-center justify-center">
+                <div className="flex flex-col items-center text-center px-9 w-lg">
+                  <h1 className="text-4xl font-bold text-white py-3 font-roboto">How to Build <br /> a New Habit</h1>
+                  <p className="text-white mt-2  py-2  ">This is essential for making progress <br /> in your health, happiness and your life</p>
+                  <button className="bg-white text-blue-500 rounded px-4 py-2 mt-4 font-medium"> + Add New Habit</button>
+                </div>
+
+                <div className="flex-shrink-0">
+                  <img src="/Colony.png" alt="Habit illustration" className="h-90 mt-29 py-3 object-contain" />
+                </div>
+              </div>
+
+
+            <div className="bg-red-200 h-80 w-[300px] rounded-xl flex items-center justify-center">
               <h1>Box 2</h1>
             </div>
 
           </div>
 
           {/* Second row: Box 3 spans the same total width */}
-          <div className="bg-red-200 h-80 w-[1000px] rounded flex items-center justify-center">
+          <div className="bg-red-200 h-80 w-[1000px] rounded-xl flex items-center justify-center">
             <h1>Box 3</h1>
           </div>
         </div>
